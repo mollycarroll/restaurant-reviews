ee2614f596cf72e91ccd252ab2391c2249de8b9a
Molly Carroll <mollyajcarroll@gmail.com>
2021-05-17 10:05:21 -0500

    add CRUD methods to reviews controller

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import RestaurantsController from './restaurants.controller.js'
+import ReviewsController from './reviews.controller.js'
 
 const router = express.Router()
 
 router.route('/').get(RestaurantsController.apiGetRestaurants)
+
+// review route for users reviewing restaurants
+router
+    .route('/review')
+    .post(ReviewsController.apiPostReview)
+    .put(ReviewsController.apiUpdateReview)
+    .delete(ReviewsController.apiDeleteReview)
 
 export default router
