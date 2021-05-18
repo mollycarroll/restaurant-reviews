16c03ae221119f8bdbaea80adb69ff68ac523b6c
Molly Carroll <mollyajcarroll@gmail.com>
2021-05-18 14:38:58 -0500

    add get restaurant by ID for show page and get restaurant cuisines methods to restaurants controller

--- backend/api/restaurants.controller.js
+++ backend/api/restaurants.controller.js
@@ -34,4 +34,33 @@ export default class RestaurantsController {
         // send response as json to whoever called the URL
         res.json(response)
     }
+
+    static async apiGetRestaurantById(req, res, next) {
+        try {
+            // looks for the restaurant ID param in URL after slash
+            let id = req.params.id || {}
+            let restaurant = await RestaurantsDAO.getRestaurantByID(id)
+
+            if (!restaurant) {
+                res.status(404).json({ error: 'not found' })
+                return
+            }
+            res.json(restaurant)
+
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
+    }
+
+    static async apiGetRestaurantCuisines(req, res, next) {
+        try {
+            let cuisines = await RestaurantsDAO.getCuisines()
+            res.json(cuisines)
+
+        } catch (e) {
+            console.log(`api, ${e}`)
+            res.status(500).json({ error: e })
+        }
+    }
 }
--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -5,7 +5,11 @@ import ReviewsController from './reviews.controller.js'
 const router = express.Router()
 
 router.route('/').get(RestaurantsController.apiGetRestaurants)
+
+// shows info about specific restaurant incl. reviews associate with it
 router.route('/id/:id').get(RestaurantsController.apiGetRestaurantById)
+
+// return a list of cuisines for dropdown menu on frontend of cuisines
 router.route('/cuisines').get(RestaurantsController.apiGetRestaurantCuisines)
 
 // review route for users reviewing restaurants
