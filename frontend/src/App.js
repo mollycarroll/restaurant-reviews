9e6193afbe091038167be5a9f24d5b1fc1ef6491
Molly Carroll <mollyajcarroll@gmail.com>
2021-05-23 12:44:46 -0500

    add 4 components and import to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Switch, Router, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import AddReview from './components/AddReview';
+import Restaurant from './components/Restaurants';
+import RestaurantsList from './components/RestaurantsList';
+import Login from './components/Login';
+
 function App() {
   return (
     <div className="App">
