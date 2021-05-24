2cbc1c3f25ce41d5e6e356211cb8261ed66d1942
Molly Carroll <mollyajcarroll@gmail.com>
2021-05-24 11:00:07 -0500

    add navbar to App.js with login/logout ternary links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,32 @@ import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <nav className='navbar navbar-expand navbar-dark bg-dark'>
+        <a href='/restaurants' className='navbar-brand'>
+          Restaurant Reviews
+        </a>
+        <div className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <Link to={'/restaurants'} className='nav-link'>
+              Restaurants
+            </Link>
+          </li>
+
+          <li className='nav-item'>
+            { user ? (
+              <a onClick={logout} className='nav-link' style={{cursor:'pointer'}}>
+                Logout {user.name}
+              </a>
+            ) : (
+              <Link to={'/login'} className='nav-link'>
+                Login
+              </Link>
+            )}
+          </li>
+
+        </div>
+      </nav>
     </div>
   );
 }
